d11a3d7a169fc38e66dcb90c0b1f21c3b839479d
Iliyana Stoyanova <iliyana.t.stoyanova@gmail.com>
2022-06-28 10:52:52 +0300

    Added search option

--- src/main/java/com/iliyana/userapplication/repository/UserRepo.java
+++ src/main/java/com/iliyana/userapplication/repository/UserRepo.java
@@ -1,7 +1,16 @@
 package com.iliyana.userapplication.repository;
 
 import com.iliyana.userapplication.model.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u" +
+            " WHERE u.firstName LIKE CONCAT('%',:item, '%')" +
+            " OR u.lastName LIKE CONCAT('%', :item, '%')")
+    List<User> filterUsers(String item, Sort sort);
 }
